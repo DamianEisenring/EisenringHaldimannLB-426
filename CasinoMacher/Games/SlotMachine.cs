e1154986bf3d06fe563b50d5e7049c6c7371d9cb
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-04-30 10:36:16 +0200

    First User Story done
    Now you can select between slots and roulette

---
+++ CasinoMacher/Games/SlotMachine.cs
@@ -0,0 +1,17 @@
+﻿using CasinoMacher.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasinoMacher.Games
+{
+    class SlotMachine : ICasinoGame
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing Slot Machine...");
+        }
+    }
+}
--- CasinoMacher/Program.cs
+++ CasinoMacher/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace CasinoMacher
+﻿using CasinoMacher.Games;
+using CasinoMacher.Interface;
+
+namespace CasinoMacher
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Benutzer kann zwischen Slots und Roulette wählen
+            Console.WriteLine("Choose a game:");
+            Console.WriteLine("1. Slots");
+            Console.WriteLine("2. Roulette");
+            Console.Write("Enter your choice: ");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            ICasinoGame game;
+
+            // Je nach Benutzerwahl wird das entsprechende Spiel erstellt
+            switch (choice)
+            {
+                case 1:
+                    game = new SlotMachine();
+                    break;
+                case 2:
+                    game = new Roulette();
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Defaulting to Slots.");
+                    game = new SlotMachine();
+                    break;
+            }
+
+            // Das ausgewählte Spiel wird gestartet
+            game.Play();
         }
     }
 }
